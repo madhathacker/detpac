af77a37639ec4cd9072cba7d74da92c16fffc8a3
Andrew <acbuckaroo@gmail.com>
2020-04-30 14:57:24 -0400

    Gitignore Data Set, Colors, and Multigraph

--- extract_features.py
+++ extract_features.py
@@ -5,24 +5,39 @@ import pandas as pd
 from clusterfck_pefile import e_features
 from plot_features import feature_plot
 
-def plot_feature(name):
-	feature = df[name]
-	#print(file_sizes)
-	feature_plot(feature)
+def plot_features(names):
+	features = []
+	for name in names:
+		features.append(df[name])
+	feature_plot(features)
+	return
 
-features_list = []
+def get_features(file_directory):
+	features_list = []
+	file_list = glob.glob(file_directory + '/*')
+	for filename in file_list:
+		if os.path.isfile(filename):
+			#print(filename)
+			#print(e_features(filename))
+			file_features = e_features(filename)
+			if "packed" in filename:
+				file_features["Packed"] = 1
+			else:
+				file_features["Packed"] = 0
+			features_list.append(file_features)
+	return features_list
 
-file_directory = "binaries"
-file_list = glob.glob(file_directory + '/*')
-for filename in file_list:
-	if os.path.isfile(filename):
-		#print(e_features(filename))
-		features_list.append(e_features(filename))
+print("\033[34;1m Getting features of unpacked binaries... \033[39;0m")
+unpacked_features_list = get_features("dataset/binaries")
+print("\033[34;1m Getting features of packed binaries... \033[39;0m")
+packed_features_list = get_features("dataset/packed")
+feature_list = unpacked_features_list + packed_features_list
 
+print("\033[34;1m Generating DataSet... \033[39;0m")
 df = pd.DataFrame.from_records(features_list)
-df['Packed'] = False
 print(df)
 
+print("\033[34;1m Saving Dataset to File... \033[39;0m")
 df.to_csv('dataset.csv', sep=',', encoding='utf-8')
 
-plot_feature('Size')
+plot_features(['Size', 'TimeDateStamp'])
--- plot_features.py
+++ plot_features.py
@@ -3,8 +3,13 @@ import matplotlib.pyplot as plt
 sns.set()
 sns.set(color_codes=True)
 
-def feature_plot(x):
-	ax = sns.distplot(x, kde=False, rug=False)
+def feature_plot(features):
+	plt.figure(figsize=(sizeof(features)*3, 3))
+	pos = 0
+	for feature in features:
+		plt.subplot(1+sizeof(features)+pos)
+		pos += 1
+		ax = sns.distplot(features, kde=False, rug=False)
 	plt.show()
 	# plt.savefig("mygraph.png")
 	return
